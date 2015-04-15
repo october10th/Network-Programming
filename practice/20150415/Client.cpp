cc38e4f78e72a53a7fe366a775c56e1c00a6d497
october10th <venitdubitare@gmail.com>
2015-04-15 21:33:22 +0800

    OAOAOAOAOAOAO

---
+++ practice/20150415/Client.cpp
@@ -0,0 +1,76 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <algorithm>
+#define MAXLINE 4096
+#define SERV_PORT 7122
+using namespace std;
+
+typedef struct sockaddr SA;
+
+void str_cli(FILE *fp, int sockfd) {
+	int maxfdp1, stdineof, n;
+	fd_set  rset;
+	char sendline[MAXLINE], recvline[MAXLINE];
+	stdineof = 0; /*use for test readable*/
+	FD_ZERO(&rset);
+	for ( int i=0; 1;i++ ) {
+		if (stdineof == 0){
+			FD_SET(fileno(fp), &rset);
+			FD_SET(sockfd, &rset);
+			maxfdp1 = max(fileno(fp), sockfd) + 1;
+			select(maxfdp1, &rset, NULL, NULL, NULL);
+			if (FD_ISSET(sockfd, &rset)) { /* socket is readable */
+				if ((n=read(sockfd, recvline, MAXLINE))== 0) {
+					if (stdineof == 1)
+						return; /* normal termination */
+					else
+						printf("str_cli: server terminated prematurely");
+				}
+				recvline[n]='\0';
+				printf("from server:\n%s\n", recvline);
+				fputs(recvline, stdout);
+				//
+				// stdineof = 1;
+				// shutdown(sockfd, SHUT_WR);//send FIN
+				// // FD_CLR(fileno(fp), &rset);
+					
+			}
+			if (FD_ISSET(fileno(fp), &rset)) { /* input is readable */
+				if (fgets(sendline, MAXLINE, fp) == NULL) { //EOF
+					stdineof = 1;
+					shutdown(sockfd, SHUT_WR);//send FIN
+					FD_CLR(fileno(fp), &rset);
+					continue;
+				}	
+				
+				
+				write(sockfd, sendline, strlen(sendline));
+				shutdown(sockfd, SHUT_WR);//send FIN OAOAOAOAOAAO
+				
+			}
+		}
+	}
+}
+int main(int argc, char **argv){
+	int sockfd;
+	struct sockaddr_in servaddr;
+	if (argc!=2)puts("usage: tcpcli <IPaddress>");
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERV_PORT);
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+	str_cli(stdin, sockfd); 
+	exit(0);
+	/* do it all */
+}
---
+++ practice/20150415/Server.cpp
@@ -0,0 +1,97 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <algorithm>
+#define LISTENQ 1024
+#define MAXLINE 4096
+#define SERV_PORT 7122
+using namespace std;
+typedef struct sockaddr SA;
+
+int main(int argc, char **argv) {
+	int i, maxi, maxfd, listenfd, connfd, sockfd;
+	int nready, client[FD_SETSIZE];
+	ssize_t n;
+	char hell[50]="hello";
+	fd_set rset, allset;
+	char line[MAXLINE];
+	socklen_t clilen;
+	struct sockaddr_in cliaddr, servaddr;	
+	
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(SERV_PORT);
+
+	bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+	listen(listenfd, LISTENQ);
+	maxfd = listenfd; /* initialize */
+	maxi = -1; /* index into client[] array */ for (i = 0; i < FD_SETSIZE; i++)
+	client[i] = -1; /* -1 indicates available entry */
+	FD_ZERO(&allset); FD_SET(listenfd, &allset);
+	for ( ; ; ) {
+		rset = allset; /* structure assignment */
+		nready = select(maxfd+1, &rset, NULL, NULL, NULL);
+		if (FD_ISSET(listenfd, &rset)) { /* new client connection */
+			clilen = sizeof(cliaddr);
+			connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen);
+			for (i = 0; i < FD_SETSIZE; i++)
+				if (client[i] < 0) {
+					client[i] = connfd; /* save descriptor */
+					break;
+				}
+			if (i == FD_SETSIZE)
+				printf("too many clients");
+
+			FD_SET(connfd, &allset);
+			if (connfd > maxfd)
+				maxfd = connfd;
+			if (i > maxi)
+				maxi = i;
+			if (--nready <= 0)
+				continue;
+		}
+
+		for (i = 0; i <= maxi; i++) {
+			if ( (sockfd = client[i]) < 0)
+				continue;
+			if (FD_ISSET(sockfd, &rset)) {
+				if ( (n = read(sockfd, line, MAXLINE)) == 0) {
+
+					puts("XD");
+					/* connection closed by client */
+					for(int i=0;i<10;i++){
+						// puts("send Hello");
+						printf("send %s\n", hell);
+						write(sockfd, hell, strlen(hell));
+						sleep(1);
+					}
+					close(sockfd);
+					FD_CLR(sockfd, &allset);
+					client[i] = -1;
+				}
+				else{
+					puts("from client");
+					line[n]='\0';
+					n=strlen(line);
+					puts(line);
+					write(sockfd, line, n);
+				}
+				if (--nready <= 0)
+					break;
+				/* no more readable descriptors */
+			}
+		}
+
+	}
+}
